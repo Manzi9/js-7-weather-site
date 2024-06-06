103c71e28287f256fcd71f778dbd0444069d9ea4
Your Name <manzi5000@hotmail.co.uk>
2024-06-06 21:39:54 +0100

    first function

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function error(error) {
 
 //go get the weather data from the api
 async function getWeather(latitude, longitude) {
-  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=17a3e02a9cc47ed1eac90bc2f9c0012a`;
+  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=025d6d7082992c91d55137fa52f388c1`;
   console.log(url);
 
   //get data and turn into object, two lectures about this world to come
@@ -23,4 +23,17 @@ async function getWeather(latitude, longitude) {
   console.log(result.city, result.list);
 }
 // Data on site
-document.getElementById("root").innerHTML = "Bob"; //
+// document.getElementById("root").innerHTML = "Bob,"; //shouldn't do it this way.
+
+function generateHTML(tag, text) {
+  let _text = document.createTextNode(text);
+  let _elem = document.createElement(tag);
+  _elem.append(_text);
+
+  return _elem;
+}
+
+document.getElementById("root").append(generateHTML("h1", "Hello World"));
+document
+  .getElementById("root")
+  .append(generateHTML("p", "this is a paragraph undeneath"));
