68dad202be3fcc8216e5b0b07155791dd58d91cd
Your Name <manzi5000@hotmail.co.uk>
2024-06-10 21:54:03 +0100

    started search bar

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+//--------------------------------------------------------------------
 //initiate get location
+//--------------------------------------------------------------------
 const options = { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 };
 navigator.geolocation.getCurrentPosition(success, error, options);
 
 const rootRef = document.getElementById("root");
-
+//--------------------------------------------------------------------
 //callbacks for success and error
+//--------------------------------------------------------------------
 function success({ coords }) {
   const { latitude, longitude } = coords;
   getweatherURL(latitude, longitude);
@@ -13,13 +16,14 @@ function error(error) {
   console.log("Error", error);
 }
 
+//--------------------------------------------------------------------
 //go get the weather data from the api
-
+//--------------------------------------------------------------------
 async function getweatherURL(latitude, longitude) {
   const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=025d6d7082992c91d55137fa52f388c1`;
   console.log(url);
 
-  //get data and turn into object, two lectures about this world to come
+  //get data and turn into object,
   let result = await fetch(url);
   result = await result.json();
 
@@ -28,6 +32,9 @@ async function getweatherURL(latitude, longitude) {
 
   console.log(result); //what's the difference between result.city and weather Array?!
 }
+//--------------------------------------------------------------------
+//UPDATE INTERFACE
+//--------------------------------------------------------------------
 function displayWeatherInfo(weatherArr) {
   rootRef.append(generateHTML("h1", `Weather in ${weatherArr.city.name}`));
   for (let i = 0; i < weatherArr.list.length; i++) {
@@ -42,6 +49,7 @@ function displayWeatherInfo(weatherArr) {
     rootRef.append(
       generateHTML("p", weatherArr.list[i].weather[0].description)
     );
+
     // rootRef.append(generateHTML("img", weatherArr.list[i].weather[0].icon));
     //
   }
@@ -49,6 +57,9 @@ function displayWeatherInfo(weatherArr) {
 // Data on site
 // document.getElementById("root").innerHTML = "Bob,"; //shouldn't do it this way.
 
+//--------------------------------------------------------------------
+//Writes HTML
+//--------------------------------------------------------------------
 function generateHTML(tag, text) {
   let _text = document.createTextNode(text); //Why does the order of text/elem variables matter?
   let _elem = document.createElement(tag);
@@ -57,3 +68,33 @@ function generateHTML(tag, text) {
 
   return _elem;
 }
+
+// const form = document.getElementById("form");
+// const city = document.getElementById("query");
+// const google = "https://www.google.com/search?q=site%3A+";
+// const site = "pagedart.com";
+
+// function submitted(event) {
+//   event.preventDefault();
+//   const url = google + site + "+" + city.value;
+//   const win = window.open(url, "_blank");
+//   win.focus();
+// }
+
+// form.addEventListener("submit", submitted);
+
+//--------------------------------------------------------------------
+//SEARCH BAR
+//--------------------------------------------------------------------
+const form = document.getElementById("form");
+const city = document.getElementById(query.value);
+const searchBTN = document.getElementById("SearchBTN");
+
+function searchForCity(city) {
+  const searchURL = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=025d6d7082992c91d55137fa52f388c1`;
+  console.log(searchURL);
+}
+form.addEventListener("submit", searchForCity);
+// searchBTN.addEventListener("click", function (e) {
+//   searchForCity(city);
+// });
